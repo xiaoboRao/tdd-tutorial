6437cb406a1cdc007fb5263db8037e59f069ead0
Bob <731653765@qq.com>
2023-06-23 17:22:00 +0800

    feat: add todo list componnent

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import { Todo } from './components/Todo'
+import { TodoForm } from './components/TodoForm'
+import { TodoType } from './types'
+import { toggleOneTodo } from './utils/toggleOneTodo'
+import './App.css'
 
 function App() {
+  const [todos, setTodos] = useState<TodoType[]>([
+    {
+      text: 'Learn about React',
+      isCompleted: false,
+    },
+    {
+      text: 'Meet friend for lunch',
+      isCompleted: false,
+    },
+    {
+      text: 'Build really cool todo app',
+      isCompleted: false,
+    },
+  ])
+
+  const addTodo = (text: string) => {
+    const newTodos = [...todos, { text }]
+    setTodos(newTodos)
+  }
+
+  const toggleTodo = (index: number) => {
+    const newTodos = toggleOneTodo(todos, index)
+    setTodos(newTodos)
+  }
+
+  const removeTodo = (index: number) => {
+    const newTodos = [...todos]
+    newTodos.splice(index, 1)
+    setTodos(newTodos)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} toggleTodo={toggleTodo} removeTodo={removeTodo} />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Todo.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { TodoType } from '../types'
+
+export function Todo({
+  todo,
+  index,
+  toggleTodo,
+  removeTodo,
+}: {
+  todo: TodoType
+  index: number
+  toggleTodo: (index: number) => void
+  removeTodo: (index: number) => void
+}) {
+  const toggleText = todo.isCompleted ? 'Redo' : 'Complete'
+  return (
+    <div className="todo" data-testid="todo-item" style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}>
+      {todo.text}
+
+      <div>
+        <button onClick={() => toggleTodo(index)}>{toggleText}</button>
+        <button data-cy="remove" data-testid="remove-todo" onClick={() => removeTodo(index)}>
+          x
+        </button>
+      </div>
+    </div>
+  )
+}
