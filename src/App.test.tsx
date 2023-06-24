d740769bf9f141fb1a69c6596bfe38bc34dffce4
Bob <731653765@qq.com>
2023-06-24 14:08:20 +0800

    feat: testing the integration about functionality of Todo and TodoForm

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,50 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, within, act } from '@testing-library/react'
+import App from './App'
+import userEvent from '@testing-library/user-event'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('should render todo list', () => {
+  // given
+  render(<App />)
+  // when
+  const todos = screen.getAllByTestId('todo-item')
+  // then
+  expect(todos).toHaveLength(3)
+})
+
+test('should add todo', () => {
+  // given
+  render(<App />)
+  act(() => {
+    // when
+    userEvent.type(screen.getByPlaceholderText("What's your plan?"), 'buy milk{enter}')
+  })
+  // then
+  expect(screen.getAllByTestId('todo-item')).toHaveLength(4)
+})
+
+test('should remove todo', () => {
+  // given
+  render(<App />)
+
+  act(() => {
+    // when
+    userEvent.click(within(screen.getByText('Learn about React')).getByTestId('remove-todo'))
+  })
+  // then
+  expect(screen.getAllByTestId('todo-item')).toHaveLength(2)
+})
+
+test('should toggle todo', () => {
+  // given
+  render(<App />)
+
+  let firstTodo = within(screen.getByText('Learn about React'))
+
+  act(() => {
+    // when
+    userEvent.click(firstTodo.getByText('Complete'))
+  })
+  firstTodo = within(screen.getByText('Learn about React'))
+  // then
+  expect(firstTodo.getByText('Redo')).toBeInTheDocument()
+})
