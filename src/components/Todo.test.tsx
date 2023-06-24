09a76a12565ade3ef91105a1db6ab74620b2039e
Bob <731653765@qq.com>
2023-06-24 14:05:53 +0800

    feat: add Todo test file , testing initialize , toogle, remove item functionality

---
+++ src/components/Todo.test.tsx
@@ -0,0 +1,85 @@
+import { render, screen } from '@testing-library/react'
+import { Todo } from './Todo'
+import userEvent from '@testing-library/user-event'
+
+describe('Todo component', () => {
+  test('should render new item', () => {
+    // given
+    const todo = {
+      id: 1,
+      text: 'Hello world',
+      isCompleted: false,
+    }
+    render(<Todo todo={todo} toggleTodo={jest.fn()} removeTodo={jest.fn()} index={todo.id} />)
+    // when
+    const element = screen.getByText('Hello world')
+    // then
+    expect(element).toBeInTheDocument()
+  })
+
+  test('should render completed item', () => {
+    // given
+    const todo = {
+      id: 1,
+      text: 'Hello world',
+      isCompleted: false,
+    }
+    render(<Todo todo={todo} toggleTodo={jest.fn()} removeTodo={jest.fn()} index={todo.id} />)
+    // when
+    const element = screen.getByText('Complete')
+    // then
+    expect(element).toBeInTheDocument()
+  })
+  test('should render redo item', () => {
+    // given
+    const todo = {
+      id: 1,
+      text: 'Hello world',
+      isCompleted: true,
+    }
+    render(<Todo todo={todo} toggleTodo={jest.fn()} removeTodo={jest.fn()} index={todo.id} />)
+    // when
+    const element = screen.getByText('Redo')
+
+    //then
+    expect(element).toBeInTheDocument()
+  })
+
+  test('should toggle item when todo not completed', () => {
+    // given
+    const todo = {
+      id: 1,
+      text: 'Hello world',
+      isCompleted: false,
+    }
+
+    const toggleTodo = jest.fn()
+    render(<Todo todo={todo} toggleTodo={toggleTodo} removeTodo={jest.fn()} index={todo.id} />)
+    // when
+    const element = screen.getByText('Complete')
+    // mock the click event
+    userEvent.click(element)
+    //then
+    expect(toggleTodo).toHaveBeenCalledTimes(1)
+
+    expect(element).toBeInTheDocument()
+  })
+
+  test('should remove item', () => {
+    // given
+    const todo = {
+      id: 1,
+      text: 'Hello world',
+      isCompleted: true,
+    }
+
+    const removeTodo = jest.fn()
+    render(<Todo todo={todo} toggleTodo={jest.fn()} removeTodo={removeTodo} index={todo.id} />)
+    // when
+    const element = screen.getByText('x')
+    // mock the click event
+    userEvent.click(element)
+    //then
+    expect(removeTodo).toHaveBeenCalledTimes(1)
+  })
+})
